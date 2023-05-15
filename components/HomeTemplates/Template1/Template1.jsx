92f2554198355f2c911bd639420a2fa447e3c9cc
ahmed.elshnawy20109@gmail.com <ahmed@192.168.1.7>
2023-05-15 16:55:30 +0300

    edits

--- components/HomeTemplates/Template1/Template1.jsx
+++ components/HomeTemplates/Template1/Template1.jsx
@@ -1,7 +1,31 @@
+import { Container, Grid } from "@mui/material";
 import React from "react";
+import HeaderOne from "../../Headers/HeaderOne/HeaderOne";
+import InfoBoxOne from "../../InfoBox/InfoBoxOne/InfoBoxOne";
+import SliderOne from "../../MainSlider/SliderOne/SliderOne";
+import CategoriesSideOne from "../../CategoriesSide/CategoriesSideOne/CategoriesSideOne";
+import SaleOne from "../../Sale/SaleOne/SaleOne";
+import SaleTwo from "../../Sale/SaleTwo/SaleTwo";
+
 const Template1 = () => {
   return (
-    <h1>template</h1>
+    <>
+      <HeaderOne />
+      <Container maxWidth="xl">
+        <InfoBoxOne />
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={3}>
+            <CategoriesSideOne />
+            
+            <SaleOne />
+          </Grid>
+          <Grid item xs={12} lg={9}>
+            <SliderOne />
+            <SaleTwo />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
