89e49f3a7a34fd47d17a778f1d22a2121642f536
ahmed.elshnawy20109@gmail.com <ahmed@192.168.1.7>
2023-05-15 16:13:47 +0300

    templates intialization

--- components/HomeTemplates/Template1/Template1.jsx
+++ components/HomeTemplates/Template1/Template1.jsx
@@ -1,35 +1,10 @@
-// REDIRECTION FILES
-import langRedirection from "../../../utils/redirections/langRedirection/langRedirection";
-import routeRedirection from "../../../utils/redirections/routeRedirection/routeRedirection";
-// INTERNATIONALIZATION
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-// REDUX STUFF
-import { wrapper } from "../../../store/store";
-// MATERIAL STUFF
-import Container from "@mui/material/Container";
-
+import React from "react";
 const Template1 = () => {
   return (
-    <Container>
-    </Container>
+    <h1>template</h1>
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ req, locale, resolvedUrl }) => {
-      const languageRedirection = langRedirection(req, locale);
-      if (languageRedirection) return languageRedirection;
 
-      const routerRedirection = routeRedirection(req, resolvedUrl);
-      if (routerRedirection) return routerRedirection;
 
-      return {
-        props: {
-          ...(await serverSideTranslations(locale, ["common"])),
-        },
-      };
-    }
-);
-
-export default Cart;
+export default Template1;
